9c7a129e7a016ff3fe133c92555a64a32d077c8e
Amber Metz <ambermetz88@gmail.com>
2019-09-06 13:19:09 -0400

    Set up components and service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { EventResultsComponent } from './event-results/event-results.component';
+import { SearchComponent } from './search/search.component';
+import { ItineraryComponent } from './itinerary/itinerary.component';
+import { EventDetailsComponent } from './event-details/event-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventResultsComponent,
+    SearchComponent,
+    ItineraryComponent,
+    EventDetailsComponent
   ],
   imports: [
     BrowserModule
